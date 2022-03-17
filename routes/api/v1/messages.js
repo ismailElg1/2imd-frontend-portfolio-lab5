eb30ed7b5293f0dd3d883ffa5f4da630b38d80cd
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-17 17:23:14 +0100

    build: installed express

--- routes/api/v1/messages.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const messageController = require('../../../controllers/api/v1/messages');
-
-router.get("/", messageController.getAll);
-router.post("/", messageController.create);
-router.put('/:id', messageController.update);
-
-module.exports = router;
